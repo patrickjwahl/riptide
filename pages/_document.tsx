ee0254c0c8b0d5cbddfc818e77db005bf94abca6
Patrick Wahl <patrickjwahl@gmail.com>
2023-05-10 14:29:05 -0400

    Add Google Ads tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,18 @@ class MyDocument extends Document {
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
             {/* <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> */}
             <meta name='description' content="Caravan K9 combines modern training methods rooted in canine behavioral psychology with intuitive owner instruction to ensure that your dog will remain obedient and stress-free for their whole life."/>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-LVWGHJYCGT"></script>
+            <script dangerouslySetInnerHTML={{__html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+
+                gtag('config', 'G-LVWGHJYCGT');
+            `}}>
+                    
+            </script>
         </Head>
         <body>
           <Main />
